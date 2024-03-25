11635d196a5c436d91881a2ddde23409e8759de9
Abdallah-Azme <abdallahazme199794@gmail.com>
2024-03-26 00:39:25 +0200

    implementing shadcn ui

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="">
+      <Button color="white" className="white">
+        hi
+      </Button>
+      <header>Amazon clone</header>
+      <main></main>
+      <footer>The copy rights are reserved</footer>
+    </div>
+  );
+}
+
+export default App;
